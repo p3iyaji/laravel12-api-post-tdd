79b314a4d672511568103c195bb0426c44d53d19
Paul Iyaji <pauliyaji@MacBookPro.station>
2026-03-01 14:22:04 +0000

    Post factory updated

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
